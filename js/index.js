4ba93978d52097c21a5617e59817babda8ccee41
itsveevoo <148736496+itsveevoo@users.noreply.github.com>
2023-12-11 16:40:25 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,35 +1,36 @@
 function submitForm(button) {
-    // Prevent the default form submission
-    button.preventDefault();
+  // Prevent the default form submission
+  button.preventDefault();
 
-    // Get form data
-    const formData = new FormData(document.getElementById('order-form'));
-  
-    // Convert FormData to a plain JavaScript object
-    const formDataObject = {};
-    formData.forEach((value, key) => {
-      formDataObject[key] = value;
-    });
+  // Get form data
+  const formData = new FormData(document.getElementById('order-form'));
 
-    console.log('Form Data:', formDataObject);
+  // Convert FormData to a plain JavaScript object
+  const formDataObject = {};
+  formData.forEach((value, key) => {
+    formDataObject[key] = value;
+  });
 
-    // Make a POST request to the /customers endpoint
-    fetch('https://quaint-bikini-calf.cyclic.app/customers', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formDataObject),
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Customer created:', data);
-        // Handle success as needed
-    })
-    .catch(error => {
-        console.error('Error creating customer:', error);
-        // Handle errors
-    });
+  console.log('Form Data:', formDataObject);
+
+  // Make a POST request to the /customers endpoint
+  fetch('https://quaint-bikini-calf.cyclic.app/customers', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formDataObject),
+  })
+  .then(response => response.json())
+  .then(data => {
+      console.log('Customer created:', data);
+window.location.href = 'order.html'
+      // Handle success as needed
+  })
+  .catch(error => {
+      console.error('Error creating customer:', error);
+      // Handle errors
+  });
 }
 
 
@@ -38,51 +39,51 @@ function submitForm(button) {
 
 // GET Data for History
 document.addEventListener("DOMContentLoaded", function() {
-    // Fetch data from the /all endpoint when the page loads
-    fetch("https://quaint-bikini-calf.cyclic.app/all")
-      .then(response => response.json())
-      .then(data => {
-        if (data.success) {
-          // Display the customer data in the container
-          console.log(data)
-          displayCustomerData(data.data);
-        } else {
-          console.error("Error fetching customer data:", data.error);
-        }
-      })
-      .catch(error => console.error("Error fetching customer data:", error));
-  
-    // Function to display customer data
-    function displayCustomerData(customers) {
-      const container = document.querySelector(".order-box");
-      
-
-      // Check if the container element exists
-      if (!container) {
-        console.error("Container element not found.");
-        return;
+  // Fetch data from the /all endpoint when the page loads
+  fetch("https://quaint-bikini-calf.cyclic.app/all")
+    .then(response => response.json())
+    .then(data => {
+      if (data.success) {
+        // Display the customer data in the container
+        console.log(data)
+        displayCustomerData(data.data);
+      } else {
+        console.error("Error fetching customer data:", data.error);
       }
-      
-      // Clear existing content
-      container.innerHTML = "";
-      
-      // Iterate through each customer and display relevant information
-      customers.forEach(customer => {
+    })
+    .catch(error => console.error("Error fetching customer data:", error));
 
-        const checkInDate = new Date(customer.check_in).toLocaleDateString();
-        const checkOutDate = new Date(customer.check_out).toLocaleDateString();
+  // Function to display customer data
+  function displayCustomerData(customers) {
+    const container = document.querySelector(".order-box");
+    
 
-        const customerElement = document.createElement("div");
-        customerElement.innerHTML = `
-          <p class="customer-info">ID: ${customer.id}</p>
-          <p class="customer-info">Name: ${customer.first_name} ${customer.last_name}</p>
-          <p class="customer-info">Check-in: ${checkInDate}</p>
-          <p class="customer-info">Check-out: ${checkOutDate}</p>
-          <hr class="customer-info">
-        `;
-        // Add a class to the container to style it if needed
-        container.classList.add("customer-container");
-        container.appendChild(customerElement);
-      });
+    // Check if the container element exists
+    if (!container) {
+      console.error("Container element not found.");
+      return;
     }
-  });
+    
+    // Clear existing content
+    container.innerHTML = "";
+    
+    // Iterate through each customer and display relevant information
+    customers.forEach(customer => {
+
+      const checkInDate = new Date(customer.check_in).toLocaleDateString();
+      const checkOutDate = new Date(customer.check_out).toLocaleDateString();
+
+      const customerElement = document.createElement("div");
+      customerElement.innerHTML = `
+        <p class="customer-info">ID: ${customer.id}</p>
+        <p class="customer-info">Name: ${customer.first_name} ${customer.last_name}</p>
+        <p class="customer-info">Check-in: ${checkInDate}</p>
+        <p class="customer-info">Check-out: ${checkOutDate}</p>
+        <hr class="customer-info">
+      `;
+      // Add a class to the container to style it if needed
+      container.classList.add("customer-container");
+      container.appendChild(customerElement);
+    });
+  }
+});
